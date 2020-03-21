3687a052c27d70474aed5bd3b70ea3ad75fe9e9b
Pedro Fari <ped.vdf@gmail.com>
2020-03-21 15:42:16 -0400

    v1
    subindo versão 1

---
+++ Project/main.js
@@ -0,0 +1,14 @@
+//node main -> executar programa
+// cls _> limpar o terminal
+
+//1° declara uma variavel, não alterável
+const nome = 'Pedro'
+
+//2° declara uma variavel, alterável
+let idade = 17
+
+idade = 19
+
+console.log(nome)
+console.log('nome preenchido:', nome)
+console.log(`Nome: ${nome}, Idade: ${idade} `)
---
+++ Project/task_1.js
@@ -0,0 +1,18 @@
+let nome 
+let idade = 3
+let altura
+let cor 
+
+let pedro = {
+    nome: 'pedro',
+    idade: 17,
+    altura: 1.74,
+    cor: 'branca'
+
+}
+
+// pedro.idade = pedro.idade + idade 
+pedro.idade += idade
+
+console.log(pedro)
+
